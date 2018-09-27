ff4e0fa336e90cf40134245f6135b6824eed5aa0
jallen <jallen@snow.edu>
2018-09-27 08:57:35 -0600

    Add project files.

---
+++ coreconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coreconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
